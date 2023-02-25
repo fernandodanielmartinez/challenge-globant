a4c812fd72607d08068c9d3b955a978aa57602e2
Fernando Daniel Martinez <37267326+fernandodanielmartinez@users.noreply.github.com>
2023-02-25 12:22:19 -0300

    Synchronizing job job-backup-entities-data from AWS Glue at 2023/02/25 15:22:19

--- job-backup-entities-data/job-backup-entities-data.py
+++ job-backup-entities-data/job-backup-entities-data.py
@@ -1,6 +1,7 @@
 import sys
 import boto3
 import json
+import logging
 
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -15,29 +16,44 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 entities_dict = ['departments', 'hired_employees', 'jobs']
 
 def get_secret():
-    session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager', region_name="us-east-1")
-    get_secret_value_response = client.get_secret_value(SecretId="MySqlCredentials")
-    return json.loads(get_secret_value_response['SecretString'])
+    try:
+        session = boto3.session.Session()
+        client = session.client(service_name='secretsmanager', region_name="us-east-1")
+        get_secret_value_response = client.get_secret_value(SecretId="MySqlCredentials")
+        return json.loads(get_secret_value_response['SecretString'])
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not get secret.")
+        logger.error(e)
 
 def read_data_from_sql(entity, secret):
-    return spark.read \
-                .format("jdbc") \
-                .option("driver", 'com.mysql.cj.jdbc.Driver') \
-                .option("url", 'jdbc:mysql://{}:{}/{}'.format(secret['host'], secret['port'], secret['db'])) \
-                .option("dbtable", entity.upper()) \
-                .option("user", secret['user']) \
-                .option("password", secret['passw']) \
-                .load()
+    try:
+        return spark.read \
+                    .format("jdbc") \
+                    .option("driver", 'com.mysql.cj.jdbc.Driver') \
+                    .option("url", 'jdbc:mysql://{}:{}/{}'.format(secret['host'], secret['port'], secret['db'])) \
+                    .option("dbtable", entity.upper()) \
+                    .option("user", secret['user']) \
+                    .option("password", secret['passw']) \
+                    .load()
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not get data from sql.")
+        logger.error(e)
     
 def write_to_s3(entity, df):
     s3_bucket = 's3-challenge-globant-backup'
-    df.write \
-      .format('avro') \
-      .save('s3a://{}/{}/'.format(s3_bucket, entity))
+    try:
+        df.write \
+          .format('avro') \
+          .save('s3a://{}/{}/'.format(s3_bucket, entity))
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not write to s3.")
+        logger.error(e)
     
 secret = get_secret()
 
