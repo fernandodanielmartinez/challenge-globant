5342a6930ff829499b56fd4263835eebbaaa3be7
Fernando Daniel Martinez <37267326+fernandodanielmartinez@users.noreply.github.com>
2023-02-25 13:07:41 -0300

    Synchronizing job job-load-historical-data from AWS Glue at 2023/02/25 16:07:40

--- job-load-historical-data/job-load-historical-data.py
+++ job-load-historical-data/job-load-historical-data.py
@@ -31,7 +31,7 @@ def read_csv_from_s3(entity, colNames, colTypes):
         s3_bucket = 's3-challenge-globant-landing'
         response = s3_client.get_object(Bucket=s3_bucket, Key='{}/{} (1) (1).csv'.format(entity, entity))
         return pd.read_csv(response.get('Body'), names=colNames, dtype=colTypes)
-    except Exceptions as e:
+    except Exception as e:
         logger.error("ERROR: Unexpected error: Could not read from s3.")
         logger.error(e)
     
@@ -41,14 +41,14 @@ def log_rows_with_nan_in_df(df):
         if df.size > 0:
             print('The following records have NaN values: ')
             print(df)
-    except Exceptions as e:
+    except Exception as e:
         logger.error("ERROR: Unexpected error: Could not logs rows with NaNs.")
         logger.error(e)
 
 def drop_nan_in_df(df):  
     try:
         return df.dropna()
-    except Exceptions as e:
+    except Exception as e:
         logger.error("ERROR: Unexpected error: Could not format df.")
         logger.error(e)
    
@@ -58,21 +58,21 @@ def get_secret():
         client = session.client(service_name='secretsmanager', region_name="us-east-1")
         get_secret_value_response = client.get_secret_value(SecretId="MySqlCredentials")
         return json.loads(get_secret_value_response['SecretString'])
-    except Exceptions as e:
+    except Exception as e:
         logger.error("ERROR: Unexpected error: Could not get secret.")
         logger.error(e)
 
 def set_db_conn(secret):
     try:
         return create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(secret['user'], secret['passw'], secret['host'], secret['port'], secret['db']))
-    except Exceptions as e:
+    except Exception as e:
         logger.error("ERROR: Unexpected error: Could not create engine.")
         logger.error(e)
 
 def write_to_sql(df, table_name, conn):
     try:
         df.to_sql(name=table_name, con=conn, if_exists='replace', index=False)
-    except Exceptions as e:
+    except Exception as e:
         logger.error("ERROR: Unexpected error: Could not write to sql.")
         logger.error(e)
 
