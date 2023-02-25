7fbb95ece9c973797944498eaa3b546c9d71e1e0
Fernando Daniel Martinez <37267326+fernandodanielmartinez@users.noreply.github.com>
2023-02-25 12:25:48 -0300

    Synchronizing job job-load-historical-data from AWS Glue at 2023/02/25 15:25:48

--- job-load-historical-data/job-load-historical-data.py
+++ job-load-historical-data/job-load-historical-data.py
@@ -1,9 +1,14 @@
 import sys
 import boto3
 import json
+import logging
 import pandas as pd
 import pymysql
+
 from sqlalchemy import create_engine
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 entities_dict = {
     'departments': {
@@ -21,31 +26,55 @@ entities_dict = {
 }
 
 def read_csv_from_s3(entity, colNames, colTypes):
-    s3_client = boto3.client('s3')
-    s3_bucket = 's3-challenge-globant-landing'
-    response = s3_client.get_object(Bucket=s3_bucket, Key='{}/{} (1) (1).csv'.format(entity, entity))
-    return pd.read_csv(response.get('Body'), names=colNames, dtype=colTypes)
+    try:
+        s3_client = boto3.client('s3')
+        s3_bucket = 's3-challenge-globant-landing'
+        response = s3_client.get_object(Bucket=s3_bucket, Key='{}/{} (1) (1).csv'.format(entity, entity))
+        return pd.read_csv(response.get('Body'), names=colNames, dtype=colTypes)
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not read from s3.")
+        logger.error(e)
     
 def log_rows_with_nan_in_df(df):  
-    df = df[df.isna().any(axis=1)]    
-    if df.size > 0:
-        print('The following records have NaN values: ')
-        print(df)
+    try:
+        df = df[df.isna().any(axis=1)]    
+        if df.size > 0:
+            print('The following records have NaN values: ')
+            print(df)
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not logs rows with NaNs.")
+        logger.error(e)
 
 def drop_nan_in_df(df):  
-    return df.dropna()
+    try:
+        return df.dropna()
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not format df.")
+        logger.error(e)
    
 def get_secret():
-    session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager', region_name="us-east-1")
-    get_secret_value_response = client.get_secret_value(SecretId="MySqlCredentials")
-    return json.loads(get_secret_value_response['SecretString'])
+    try:
+        session = boto3.session.Session()
+        client = session.client(service_name='secretsmanager', region_name="us-east-1")
+        get_secret_value_response = client.get_secret_value(SecretId="MySqlCredentials")
+        return json.loads(get_secret_value_response['SecretString'])
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not get secret.")
+        logger.error(e)
 
 def set_db_conn(secret):
-    return create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(secret['user'], secret['passw'], secret['host'], secret['port'], secret['db']))
+    try:
+        return create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(secret['user'], secret['passw'], secret['host'], secret['port'], secret['db']))
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not create engine.")
+        logger.error(e)
 
 def write_to_sql(df, table_name, conn):
-    df.to_sql(name=table_name, con=conn, if_exists='replace', index=False)
+    try:
+        df.to_sql(name=table_name, con=conn, if_exists='replace', index=False)
+    except Exceptions as e:
+        logger.error("ERROR: Unexpected error: Could not write to sql.")
+        logger.error(e)
 
 for key in entities_dict:
     entity_df = read_csv_from_s3(key, entities_dict[key]['colNames'], entities_dict[key]['colTypes'])
